3634d2ed22d862fbd579d8f05a5276a6d5a6e929
unknown <pengjiahui_sx@iqiyi.pps>
2019-03-21 21:50:28 +0800

    dpki dapp api. keypair generate.

--- index.js
+++ index.js
@@ -1,21 +1,33 @@
 const Web3 = require('web3')
+const BigNumber = require('bignumber.js')
 
 const contract = require('./build/contracts/DPKI.json')
 
 const w3 = new Web3(new Web3.providers.HttpProvider('http://127.0.0.1:8545'))
 
 const deployed = {
-    address: '0xbd950a85368892b3aaccb0cea1fc0b55a1daf5a4'
+    address: '0xa6cc34ab1d070c59979c51e696681cd1cdcaeff0'
 }
 
 const user = {
-    address: '0x787a5d8e1cde4b0d5190738413c60d85151e8d70'
+    address: '0x6c28d287a566caac85119443d4ae9290960dfd4d'
 }
 
 const DPKI = w3.eth.contract(contract.abi)
 const dpkiInstance = DPKI.at(deployed.address)
 
-dpkiInstance.addIdentity('fake_id', 'fake_pub', {
+// dpkiInstance.addIdentity('fake_id', 'fake_pub', {
+//     from: user.address,
+//     gas: 5000000,
+// }, (error, result) => {
+//     if (error) {
+//         console.log(error)
+//     } else {
+//         console.log(result)
+//     }
+// })
+
+dpkiInstance.getIdentity("0x6c28d287a566caac85119443d4ae9290960dfd4d", {
     from: user.address,
     gas: 5000000,
 }, (error, result) => {
