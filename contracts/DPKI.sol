5736750e697d03d15cd30b39425e1a8a3dfc7afe
tomoya01 <peng4766@gmail.com>
2019-03-25 09:33:51 +0800

    update contract API

--- contracts/DPKI.sol
+++ contracts/DPKI.sol
@@ -9,7 +9,6 @@ contract DPKI {
         uint no;
         string ipAddr;
         string pubkey;
-        string signature;
         address signer;
         string certFileHash;
     }
@@ -20,9 +19,10 @@ contract DPKI {
     uint identityNumber;
     
     event AddedIdentity(address addr);
+    event AddedSigner(address signee, address signer);
     event SignedIdentity(address signer, address signee);
-    event SavedIdentityCertFile(address owner, string filehash);
-    event UpdateIdentityInfo(address owner, string infoName, string newValue, string certFileHash);
+    // event SavedIdentityCertFile(address owner, string filehash);
+    // event UpdateIdentityInfo(address owner, string infoName, string newValue, string certFileHash);
     
     function addIdentity(string memory _ipAddr, string memory _pubkey) public {
         require(
@@ -37,7 +37,6 @@ contract DPKI {
             identityNumber,
             _ipAddr,
             _pubkey,
-            "",
             address(0),
             ""
         );
@@ -47,40 +46,53 @@ contract DPKI {
         emit AddedIdentity(msg.sender);
     }
     
-    function signIdentity(address _signee, string memory _signature) public {
+    function addSigner(address _signee) public {
         require(
-            bytes(identities[msg.sender].signature).length > 0 &&
-            bytes(identities[_signee].signature).length == 0 || 
-            identities[msg.sender].no == 0 && 
-            msg.sender == _signee,
+            identities[_signee].signer == address(0),
+            "This identity has been signed"
+        );
+        
+        identities[_signee].signer = msg.sender;
+        
+        emit AddedSigner(_signee, msg.sender);
+    }
+    
+    function signIdentity(address _signee, string memory _fileHash) public {
+        require(
+            bytes(identities[msg.sender].ipAddr).length > 0 &&
+            bytes(identities[msg.sender].pubkey).length > 0 &&
+            bytes(identities[_signee].ipAddr).length > 0 &&
+            bytes(identities[_signee].pubkey).length > 0 &&
+            identities[_signee].signer == address(0) &&
+            bytes(identities[_signee].certFileHash).length == 0 &&
+            msg.sender != _signee,
             "Illegal Signature Process"
         );
         
         identities[_signee].signer = msg.sender;
-        identities[_signee].signature = _signature;
+        identities[_signee].certFileHash = _fileHash;
         
         emit SignedIdentity(msg.sender, _signee);
     }
     
-    function saveCertFileHash(string memory _fileHash) public {
-        require(
-            bytes(identities[msg.sender].certFileHash).length == 0,
-            "You Have Owned A Certificate Already"
-        );
+    // function saveCertFileHash(string memory _fileHash) public {
+    //     require(
+    //         bytes(identities[msg.sender].certFileHash).length == 0,
+    //         "You Have Owned A Certificate Already"
+    //     );
         
-        identities[msg.sender].certFileHash = _fileHash;
+    //     identities[msg.sender].certFileHash = _fileHash;
         
-        emit SavedIdentityCertFile(msg.sender, _fileHash);
-    }
+    //     emit SavedIdentityCertFile(msg.sender, _fileHash);
+    // }
 
-    function getIdentity(address _addr) public view returns (uint, string memory, string memory, string memory, address, string memory) {
+    function getIdentity(address _addr) public view returns (uint, string memory, string memory, address, string memory) {
         require(identities[_addr].no >= 0, "This Address Has No Identity Yet");
 
         return (
             identities[_addr].no,
             identities[_addr].ipAddr,
             identities[_addr].pubkey,
-            identities[_addr].signature,
             identities[_addr].signer,
             identities[_addr].certFileHash
         );
