94033f3a4bb88ce5092a8ce46aa81b655f309556
tomoya01 <peng4766@gmail.com>
2019-05-08 11:33:21 +0800

    Douban Auth test pass

--- auth.delegation.js
+++ auth.delegation.js
@@ -1,7 +1,33 @@
 const fetch = require('node-fetch')
+const { URLSearchParams } = require('url')
+const { client_id, subject_id } = require('./users/dbkey.json')
 
-fetch('http://localhost:2000/token?addr=did:cpc:uuid:subid1')
+fetch('http://localhost:2000/token?addr=did:cpc:uuid:subid2')
 .then(res => res.json())
 .then(json => {
   console.log(json)
+  const { token } = json
+  updateComment(token)
 })
+.catch(error => {
+  console.log(`AUTH FAILED`)
+})
+
+function updateComment(token) {
+  const params = new URLSearchParams();
+  params.append('client_id', client_id);
+  params.append('subject_id', subject_id);
+  params.append('comment', Date.now()+' MARKING TEST');
+  params.append('status', 'wish');
+
+  // const headers = new fetch.Headers();
+  // headers.append("Authorization", `Bearer ${token}`);
+  fetch('https://api.douban.com/v2/movie/collection/1226523442', {
+    method: 'POST',
+    body: params,
+    headers: {
+      "Authorization": `Bearer ${token}`
+    }
+  }).then(res => res.json())
+  .then(json => console.log(json))
+}
